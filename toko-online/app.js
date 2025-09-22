0f2e54ccc7d3868327f9b6a8bf2d744003f5cb39
NabilaSyahwalrani <nabilasyahwalrani_2327240103@mhs.mdp.ac.id>
2025-09-22 17:26:18 +0700

    Pertemuan 4 -Toko Online

--- toko-online/app.js
+++ toko-online/app.js
@@ -6,11 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var engine = require('ejs-blocks'); // menggunakan ejs-blocks
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine); // daftarkan ejs-blocks sebagai view engine
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -22,9 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Halaman Home' });
-});
+//serving bootstrap
+app.use('/bootstrap', express.static(path.join(__dirname + '/node_modules/bootstrap/dist')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
